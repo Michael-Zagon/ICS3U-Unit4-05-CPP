7734e1f452e22103268584843faedfc7a4393f4f
Michael <ubuntu@ip-172-31-7-8.ec2.internal>
2021-10-07 01:18:21 +0000

    loop addition thing

---
+++ loop_addition.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 Michael Zagon All rights reserved
+
+// Created by: Michael Zagon
+// Created on: September 2021
+// This is the loop addition program
+
+#include <iostream>
+
+int main() {
+    // This is the loop addition function
+
+    int times_added;
+    int positive_integer;
+    int counter;
+    int total = 0;
+
+    // Input, Process and Output
+    std::cout << "How many positive numbers would you like to add: ";
+    std::cin >> times_added;
+    std::cout << "" << std::endl;
+    if (times_added == 0) {
+        std::cout << "Invalid Input." << std::endl;
+    }
+    try {
+        for (counter = 0; counter < times_added; counter++) {
+            std::cout << "Enter a positive integer to add: ";
+            std::cin >> positive_integer;
+            std::cout << "" << std::endl;
+            if (positive_integer < 0) {
+                continue;
+            } else if (positive_integer > 0) {
+                total = total + positive_integer;
+            } else if (positive_integer == 0) {
+                std::cout << "Invalid Input." << std::endl;
+                break;
+            }
+        }
+        if (positive_integer > 0 || positive_integer < 0) {
+            std::cout << "Sum of all positive integers is " << total
+            << std::endl;
+    }
+    }
+    catch (std::invalid_argument) {
+            std::cout << "Invalid Input." << std::endl;
+        }
+    std::cout << "\nDone." << std::endl;
+}
